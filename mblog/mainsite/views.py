a727603b79d63418022b1f7daf5e41bb3561f9df
Cho-Chun Huang <cchuang4@gmail.com>
2019-07-17 07:33:12 +0800

    base blog html commit

--- mblog/mainsite/views.py
+++ mblog/mainsite/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from datetime import datetime
+from django.shortcuts import render, redirect
+#from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
+def homepage(request):
+    posts = Post.objects.all()
+    now = datetime.now()
+    return render(request, 'index.html', locals())
+"""    post_lists = list()
+    for count, post in enumerate(posts):
+        post_lists.append("No.{}:".format(str(count)) + str(post) + "<hr>")
+        post_lists.append("<small>" + post.body + "</small><br><br>") #str(post.body.encode('utf-8'))
+    return HttpResponse(post_lists)
+"""
+
+def showpost(request, slug):
+    try:
+        post = Post.objects.get(slug = slug)
+        if post != None:
+            return render(request, 'post.html', locals())
+    except:
+        return redirect('/')
