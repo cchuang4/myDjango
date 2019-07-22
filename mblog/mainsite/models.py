faa4bad37321ec489410fb0a55cdead96cd463f0
Cho-Chun Huang <cchuang4@gmail.com>
2019-07-22 17:05:12 +0800

    Model NewTable commit

--- mblog/mainsite/models.py
+++ mblog/mainsite/models.py
@@ -16,3 +16,14 @@ class Post(models.Model):
     def __str__(self):
         return self.title
         
+class NewTable(models.Model):
+    bigint_f = models.BigIntegerField()
+    bool_f = models.BooleanField()
+    date_f = models.DateField(auto_now=True)
+    char_f = models.CharField(max_length=20, unique=True)
+    datetime_f = models.DateField(auto_now_add=True)
+    decimal_f = models.DecimalField(max_digits=10, decimal_places=2)
+    float_f = models.FloatField(null=True)
+    int_f = models.IntegerField(default=2010)
+    text_f = models.TextField()
+    
