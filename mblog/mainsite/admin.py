fcf89075d4cf69902605a456588c808133f98c9a
Cho-Chun Huang <cchuang4@gmail.com>
2019-07-22 17:19:43 +0800

    model modifying commit

--- mblog/mainsite/admin.py
+++ mblog/mainsite/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Post
+from .models import NewTable, Product
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'pub_date')
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(NewTable)
+admin.site.register(Product)
--- mblog/mainsite/models.py
+++ mblog/mainsite/models.py
@@ -27,3 +27,14 @@ class NewTable(models.Model):
     int_f = models.IntegerField(default=2010)
     text_f = models.TextField()
     
+class Product(models.Model):
+    SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    sku = models.CharField(max_length=5)
+    name = models.CharField(max_length=20)
+    price = models.PositiveIntegerField()
+    size = models.CharField(max_length=1, choices=SIZES)
+
